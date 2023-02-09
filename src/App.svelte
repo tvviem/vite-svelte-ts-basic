d459d5102e916e34cf8258abf7d6de799e13161e
tvviem <trieuvinhviem@gmail.com>
2023-02-09 20:48:33 +0700

    3.1 - conditional rendering using #if control

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,18 @@
 <script lang="ts">
-  const name = 'Vinh Nam';
-  const htmlString = '<b style="color:red">BOLD TEXT</b>'; // strictly
-  const hack = `<a href="#" onclick="alert('You have been hacked!')">Win the prize</a>`; // not use
-  const id = 'heading'; // auto same attribute
-  const disabled = true;
-  const status = 'success';
-  const isPromoted = true;
-  const promoted = true; // for shortstyle
+  const num: number = +87;
 </script>
 
 <main>
-  <!-- <h1>Hello {name}</h1>
-  <p>HTML BINDING {@html htmlString}</p>
-  <h2>{@html hack}</h2>
-  <h2 {id}>This is a heading</h2>
-  <button {disabled}>BindAttr</button> -->
-  <h2 class="underline">Underlined text</h2>
-  <h2 class={status}>STATUS</h2>
-  <h2 class={isPromoted ? 'promoted' : ''}>Movie title</h2>
-  <h2 class:promoted={isPromoted}>Movie title using directive class css</h2>
-  <h2 class:promoted>Movie title using directive class css - SHORT style</h2>
+  {#if num === 0}
+    <h2>THE NUMBER IS ZERO</h2>
+  {:else if num < 0}
+    <h2>THE NUMBER IS NAGATIVE</h2>
+  {:else if num > 0}
+    <h2>THE NUMBER IS POSITIVE</h2>
+  {:else}
+    <h2>not a number</h2>
+  {/if}
 </main>
 
 <style>
-  .underline {
-    text-decoration: underline;
-  }
-  .danger {
-    color: red;
-  }
-  .success {
-    color: olivedrab;
-  }
-  .promoted {
-    font-style: italic;
-  }
 </style>
