39a8c8864c5c68827a6b20a9ae8bb67f4c2af3d3
tvviem <trieuvinhviem@gmail.com>
2023-02-09 20:26:47 +0700

    2.1 - Tag Attributes binding

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,16 @@
   const name = 'Vinh Nam';
   const htmlString = '<b style="color:red">BOLD TEXT</b>'; // strictly
   const hack = `<a href="#" onclick="alert('You have been hacked!')">Win the prize</a>`; // not use
+  const id = 'heading'; // auto same attribute
+  const disabled = true;
 </script>
 
 <main>
   <h1>Hello {name}</h1>
   <p>HTML BINDING {@html htmlString}</p>
   <h2>{@html hack}</h2>
+  <h2 {id}>This is a heading</h2>
+  <button {disabled}>BindAttr</button>
 </main>
 
 <style>
