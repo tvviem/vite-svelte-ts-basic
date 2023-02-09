a144c5a8f0ff9ad133595df70017cbea633ffcac
tvviem <trieuvinhviem@gmail.com>
2023-02-09 20:19:23 +0700

    1 - Init project, binding text and binding html string

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const name = 'Vinh Nam';
+  const htmlString = '<b style="color:red">BOLD TEXT</b>'; // strictly
+  const hack = `<a href="#" onclick="alert('You have been hacked!')">Win the prize</a>`; // not use
+</script>
+
+<main>
+  <h1>Hello {name}</h1>
+  <p>HTML BINDING {@html htmlString}</p>
+  <h2>{@html hack}</h2>
+</main>
+
+<style>
+</style>
