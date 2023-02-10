15aca933f08d3814395c42d82a339203b2e29259
tvviem <trieuvinhviem@gmail.com>
2023-02-10 09:30:48 +0700

    3.4 - FormHandling 1 input, textarea, select, input file type

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,9 @@
 <script lang="ts">
-  let count = 0;
-
-  const increment = (event: MouseEvent) => {
-    count += 1;
-    console.log(event);
-  };
-  const incSteps = (event: MouseEvent, steps: number) => {
-    count += steps;
-    console.log(event);
-  };
-  // prototype
-  // const increment1 = (
-  //   event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }
-  // ) => {
-  //   throw new Error('Function not implemented.');
-  // };
+  import FormHandling from './lib/FormHandling.svelte';
 </script>
 
 <main>
-  <button on:click={() => count++}>way A - Count {count}</button>
-  <button on:click={increment}>way B - Count {count}</button>
-  <button on:click={(e) => increment(e)}>way C - Count {count}</button>
-  <button on:click={(e) => incSteps(e, 3)}>way D - Count + steps {count}</button
-  >
+  <FormHandling />
 </main>
 
 <style>
---
+++ src/lib/FormHandling.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  let files: FileList;
+  const formValues = {
+    name: '',
+    profileSummary: '',
+    country: '',
+    jobLocation: [],
+    imgBase64: ''
+  };
+</script>
+
+<section>
+  <div>
+    <pre>
+      {JSON.stringify(formValues, null)}
+    </pre>
+  </div>
+  <form action="">
+    <div class="formContent">
+      <div class="formControl">
+        <label for="name">Name</label>
+        <input type="text" id="name" bind:value={formValues.name} />
+      </div>
+
+      <div class="formControl">
+        <label for="profile">Profile Summary</label>
+        <textarea id="profile" bind:value={formValues.profileSummary} />
+      </div>
+
+      <div class="formControl">
+        <label for="country">Your country</label>
+        <select id="country" bind:value={formValues.country}>
+          <option value="">Select a country</option>
+          <option value="Vietnam">Vietnam</option>
+          <option value="India">India</option>
+          <option value="Singapore">Singapore</option>
+        </select>
+      </div>
+
+      <div class="formControl">
+        <label for="job-location">Job Location</label>
+        <select id="job-location" bind:value={formValues.jobLocation} multiple>
+          <option value="Requirements Collection">Req Colection</option>
+          <option value="System Analysis">Sys Analysis</option>
+          <option value="Frontend">Frontend</option>
+          <option value="Backend">Backend</option>
+        </select>
+      </div>
+
+      <div class="formControl">
+        <label for="taptin">Chọn tập tin</label>
+        <input id="taptin" type="file" multiple bind:files />
+      </div>
+
+      <div class="formControl">
+        <label for="img34">Hình 3x4</label>
+        <input type="file" />
+      </div>
+    </div>
+  </form>
+  {#if files && files[0]}
+    <p>
+      {files[0].name}
+    </p>
+  {/if}
+</section>
+
+<style>
+  section {
+    margin: 5px auto;
+  }
+  .formContent {
+    display: flex;
+  }
+  .formControl {
+    margin: 20px auto;
+  }
+</style>
